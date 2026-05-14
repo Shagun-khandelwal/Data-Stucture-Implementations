8c17184786d92513c0d7c3efce14eaa97af4cda0
shagun-khandelwal <kshagun02@gmail.com>
2026-05-15 02:42:19 +0530

    feat Leetcode 733: Flood Fill using BFS

---
+++ matrix/bfs/LC-733.Flood Fill.py
@@ -0,0 +1,53 @@
+# You are given an image represented by an m x n grid of integers image, where image[i][j] represents the pixel value of the image. You are also given three integers sr, sc, and color. Your task is to perform a flood fill on the image starting from the pixel image[sr][sc].
+
+# To perform a flood fill:
+
+# Begin with the starting pixel and change its color to color.
+# Perform the same process for each pixel that is directly adjacent (pixels that share a side with the original pixel, either horizontally or vertically) and shares the same color as the starting pixel.
+# Keep repeating this process by checking neighboring pixels of the updated pixels and modifying their color if it matches the original color of the starting pixel.
+# The process stops when there are no more adjacent pixels of the original color to update.
+# Return the modified image after performing the flood fill.
+
+ 
+
+# Example 1:
+
+# Input: image = [[1,1,1],[1,1,0],[1,0,1]], sr = 1, sc = 1, color = 2
+
+# Output: [[2,2,2],[2,2,0],[2,0,1]]
+
+# Explanation:
+
+
+
+# From the center of the image with position (sr, sc) = (1, 1) (i.e., the red pixel), all pixels connected by a path of the same color as the starting pixel (i.e., the blue pixels) are colored with the new color.
+
+# Note the bottom corner is not colored 2, because it is not horizontally or vertically connected to the starting pixel.
+
+# Example 2:
+
+# Input: image = [[0,0,0],[0,0,0]], sr = 0, sc = 0, color = 0
+
+# Output: [[0,0,0],[0,0,0]]
+# time complexity is O(m*n) because in the worst case, we might have to visit every pixel in the image to perform the flood fill. Therefore, the overall time complexity is O(m*n).
+# space complexity is O(m*n) because in the worst case, we might have to store all the pixels in the queue for BFS. Additionally, the recursion stack for DFS can also go as deep as O(m*n) in the worst case. Therefore, the overall space complexity is O(m*n).
+from collections import deque
+from typing import List
+class Solution:
+    def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
+        original_color = image[sr][sc]
+        if original_color == color:
+            return image
+        queue = deque([(sr,sc)])
+        rows,cols = len(image),len(image[0])
+        directions = [(1,0),(0,-1),(-1,0),(0,1)]
+        image[sr][sc] = color
+        while queue:
+            for _ in range(len(queue)):
+                r,c = queue.popleft()
+                for dx,dy in directions:
+                    nx,ny = dx+r,dy+c
+                    if 0<=nx<rows and 0<=ny<cols and image[nx][ny] == original_color:
+                        image[nx][ny] = color
+                        queue.append((nx,ny))
+        return image
