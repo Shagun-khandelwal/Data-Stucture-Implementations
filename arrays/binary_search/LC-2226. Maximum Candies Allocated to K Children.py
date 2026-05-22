b5aaa2db1bc5938cda0e29dfa78c0d11c2a9ab91
shagun-khandelwal <kshagun02@gmail.com>
2026-05-22 08:14:18 +0530

    feat Leetcode 2226: Maximum Candies allocated to K children

---
+++ arrays/binary_search/LC-2226. Maximum Candies Allocated to K Children.py
@@ -0,0 +1,47 @@
+# You are given a 0-indexed integer array candies. Each element in the array denotes a pile of candies of size candies[i]. You can divide each pile into any number of sub piles, but you cannot merge two piles together.
+
+# You are also given an integer k. You should allocate piles of candies to k children such that each child gets the same number of candies. Each child can be allocated candies from only one pile of candies and some piles of candies may go unused.
+
+# Return the maximum number of candies each child can get.
+
+ 
+
+# Example 1:
+
+# Input: candies = [5,8,6], k = 3
+# Output: 5
+# Explanation: We can divide candies[1] into 2 piles of size 5 and 3, and candies[2] into 2 piles of size 5 and 1. We now have five piles of candies of sizes 5, 5, 3, 5, and 1. We can allocate the 3 piles of size 5 to 3 children. It can be proven that each child cannot receive more than 5 candies.
+# Example 2:
+
+# Input: candies = [2,5], k = 11
+# Output: 0
+# Explanation: There are 11 children but only 7 candies in total, so it is impossible to ensure each child receives at least one candy. Thus, each child gets no candy and the answer is 0.
+ 
+
+# Constraints:
+
+# 1 <= candies.length <= 105
+# 1 <= candies[i] <= 107
+# 1 <= k <= 1012
+from typing import List
+class Solution:
+    def maximumCandies(self, candies: List[int], k: int) -> int:
+        def distribution(max_candies,candies,total_childs):  # 4
+            # how many times can i distribute each pck of candy so that it can't excceeds max_candies we can give
+            child = 0
+            for candy in candies:
+                child+= candy // max_candies
+            return child >= total_childs
+        if sum(candies) < k:
+            return 0
+        start = 1
+        end = max(candies)
+        ans = max(candies)
+        while start <= end:
+            mid  = (start+end)//2
+            if distribution(mid,candies,k):
+                ans = mid
+                start = mid+1
+            else:
+                end = mid -1
+        return ans
